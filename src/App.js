204b552816b7939e6eb6bce908f715eaee9beccb
стас тареличев <tarelichev_sv@astralnalog.ru>
2023-01-06 15:59:51 +0300

    master commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import LoginForm from "./components/LoginForm";
+import "./App.css"
+import {Header} from "./components/Header/Header";
+import {Route, Routes} from "react-router-dom";
+import MainPage from "./components/Pages/MainPage";
+import {Footer} from "./components/Footer/Footer";
+import {PublicRoute} from "./components/PublicRoute/PublicRoute";
+import {PrivateRoute} from "./components/PrivateRoute/PrivateRoute";
+import {NoMatch} from "./components/NoMatch/NoMatch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
+    const [userName, setUserName] = useState(localStorage.getItem('userName'));
+
+
+    return <div className="App">
+            <Header userName={userName} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+            <hr/>
+            <main>
+                <Routes>
+                    <Route path="/login"
+                    element={
+                        <PublicRoute isLoggedIn={isLoggedIn}>
+                            <LoginForm setIsLoggedIn={setIsLoggedIn} setUserName={setUserName}/>
+                        </PublicRoute>}
+                    />
+
+                    <Route path="/*"
+                           element={
+                               <PrivateRoute isLoggedIn={isLoggedIn}>
+                                   <MainPage isLoggedIn={isLoggedIn}/>
+                               </PrivateRoute>}
+                    />
+
+                    <Route path="*"
+                           element={<NoMatch/>}
+                    />
+                </Routes>
+            </main>
+            <Footer year={new Date().getFullYear()}/>
+        </div>
+
 }
 
 export default App;
