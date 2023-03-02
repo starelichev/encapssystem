2c38a2a13bfbb49869a4cfed26c7935b6d34f866
Станислав Тареличев <tarelichev_sv@astralnalog.ru>
2023-03-02 15:56:31 +0300

    Мнемосхема готово

--- src/components/Pages/SchemePage.js
+++ src/components/Pages/SchemePage.js
@@ -15,6 +15,13 @@ import {Image} from "antd";
 
 const SchemePage = () => {
     const params = useSelector(state => state.params.params);
+    const returnPressure = params.filter(param => param.device_id === "14783441")[0]?.value;
+    const boilerCircuit = params.filter(param => param.device_id === "14783501")[0]?.value;
+    const pumpDifference = params.filter(param => param.device_id === "14783465")[0]?.value;
+    const pressureBefore = params.filter(param => param.device_id === "14783591")[0]?.value;
+    const tempFiling = params.filter(param => param.device_id === "14783567")[0]?.value;
+    const tempReturn = params.filter(param => param.device_id === "14783639")[0]?.value;
+    const rawWater = params.filter(param => param.device_id === "14783435")[0]?.value;
 
     const FetchWaterParam = params.filter(param => param.device_id === "14784365")[0]?.value;
     const[waterParam, setWaterParam] = useState("");
@@ -70,15 +77,29 @@ const SchemePage = () => {
     }, [FetchBoilerThird])
 
     return (
-        <div className={styles.divStyle}>
-            <Image src={scheme} preview={false}/>
-            <Image src={pipes} preview={false} rootClassName={styles.addImg}/>
-            <Image src={waterParam} preview={false} rootClassName={styles.addImg}/>
-            <Image src={boilerFirstParam} preview={false} rootClassName={styles.addImg}/>
-            <Image src={boilerSecondParam} preview={false} rootClassName={styles.addImg}/>
-            <Image src={boilerThirdParam} preview={false} rootClassName={styles.addImg}/>
-            <Image src={all} preview={false} rootClassName={styles.addImg}/>
-        </div>
+            <div className={styles.div}>
+                <div className={styles.tFirst}>{FetchBoilerFirst}</div>
+                <div className={styles.tSecond}>{FetchBoilerSecond}</div>
+                <div className={styles.tThird}>{FetchBoilerThird}</div>
+                <div className={styles.returnPressure}>{returnPressure}</div>
+                <div className={styles.boilerCircuit}>{boilerCircuit}</div>
+                <div className={styles.boilerCircuitStaticFirst}>3.553</div>
+                <div className={styles.boilerCircuitStaticThird}>0.291</div>
+                <div className={styles.pumpDifference}>{pumpDifference}</div>
+                <div className={styles.boilerCircuitFive}>3.870</div>
+                <div className={styles.boilerCircuitSeven}>4.967</div>
+                <div className={styles.pressureBefore}>{pressureBefore}</div>
+                <div className={styles.tempFiling}>{tempFiling}</div>
+                <div className={styles.tempReturn}>{tempReturn}</div>
+                <div className={styles.rawWater}>{rawWater}</div>
+                <Image src={scheme} preview={false}/>
+                <Image src={pipes} preview={false} rootClassName={styles.Img}/>
+                <Image src={waterParam} preview={false} rootClassName={styles.Img}/>
+                <Image src={boilerFirstParam} preview={false} rootClassName={styles.Img}/>
+                <Image src={boilerSecondParam} preview={false} rootClassName={styles.Img}/>
+                <Image src={boilerThirdParam} preview={false} rootClassName={styles.Img}/>
+                <Image src={all} preview={false} rootClassName={styles.Img}/>
+            </div>
     );
 };
 
