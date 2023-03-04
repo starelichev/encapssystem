2e97ed4ee1c1a70079fbaf704a9f2113acf11fdd
Станислав Тареличев <tarelichev_sv@astralnalog.ru>
2023-03-04 19:02:06 +0300

    Нормальная авторизация

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,20 @@ import React from "react";
 export const Header = ({ isLoggedIn, setIsLoggedIn, userName, setIsAdmin }) => {
     const name = localStorage.getItem('name')?.replace( /"/g, '');
     const lastName = localStorage.getItem('lastName')?.replace( /"/g, '');
+    const role = getRole();
+
+    function getRole() {
+        let role;
+        const item = localStorage.getItem('role')?.replace( /"/g, '');
+        if (item === "Admin") {
+            role = 'Администратор'
+        }
+        else {
+            role = 'Пользователь'
+        }
+
+        return role;
+    }
 
     const handleLogOut = () => {
         localStorage.removeItem('jwt')
@@ -24,13 +38,17 @@ export const Header = ({ isLoggedIn, setIsLoggedIn, userName, setIsAdmin }) => {
 
                 isLoggedIn ?
                         <nav style={{fontSize: "18px"}}>
-                            Добро пожаловать, &nbsp;<strong>{name}</strong>&nbsp;<strong>{lastName}</strong>
+                            <div>Добро пожаловать, &nbsp;<strong>{name}</strong>&nbsp;<strong>{lastName}</strong>
+                            <br/>
+                                <div style={{marginRight: "30px", fontSize: "15px", marginLeft: "200px"}}>{role}
+                                </div></div>
                             <NavLink
                                 onClick={handleLogOut}
                                 to="/login"
                             >
-                                <MeetingRoom />
-                                Выход
+                                <MeetingRoom style={{marginBottom: "18px"}} />
+                                <div style={{marginBottom: "14px"}}>Выход</div>
+
                             </NavLink>
                         </nav>
                     : 'Вы не авторизованы!'
