708ef3a2dc3723a62a2aa3c658074931724bf539
Станислав Тареличев <tarelichev_sv@astralnalog.ru>
2023-03-07 18:46:47 +0300

    Админка начало

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,26 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import {MeetingRoom} from "@mui/icons-material";
-import React from "react";
+import React, {useState} from "react";
+import {Button, Collapse, Divider, Input, Modal} from "antd";
+const { Panel } = Collapse;
 
 export const Header = ({ isLoggedIn, setIsLoggedIn, userName, setIsAdmin }) => {
     const name = localStorage.getItem('name')?.replace( /"/g, '');
     const lastName = localStorage.getItem('lastName')?.replace( /"/g, '');
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const role = getRole();
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
     function getRole() {
         let role;
@@ -33,22 +47,58 @@ export const Header = ({ isLoggedIn, setIsLoggedIn, userName, setIsAdmin }) => {
 
     return (
         <header className={styles.mainHeader}>
-
             {
-
                 isLoggedIn ?
                         <nav style={{fontSize: "18px"}}>
                             <div>Добро пожаловать, &nbsp;<strong>{name}</strong>&nbsp;<strong>{lastName}</strong>
+                                {role === 'Администратор' ? <Button onClick={showModal} style={{marginLeft: "20px", marginRight: "20px"}}>
+                                    Админ. панель
+                                </Button> : <></>}
+                                <Modal title="Панель администратора" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                                    <Divider orientation="left">Регистрация нового пользователя</Divider>
+                                    <Collapse>
+                                        <Panel header="Регистрация нового пользователя" key="1">
+                                            <div style={{display: "flex", height: "30px", margin: "10px"}}>
+                                                <p style={{width: "100px"}}>Имя</p>
+                                                <Input style={{marginLeft: "10px"}}/>
+                                            </div>
+                                            <div style={{display: "flex", height: "30px", margin: "10px"}}>
+                                                <p style={{width: "100px"}}>Фамилия</p>
+                                                <Input style={{marginLeft: "10px"}}/>
+                                            </div>
+                                            <div style={{display: "flex", height: "30px", margin: "10px"}}>
+                                                <p style={{width: "100px"}}>Логин</p>
+                                                <Input style={{marginLeft: "10px"}}/>
+                                            </div>
+                                            <div style={{display: "flex", height: "30px", margin: "10px"}}>
+                                                <p style={{width: "100px"}}>Пароль</p>
+                                                <Input style={{marginLeft: "10px"}}/>
+                                            </div>
+                                            <div style={{display: "flex", height: "30px", margin: "10px"}}>
+                                                <p style={{width: "100px"}}>Роль</p>
+                                                <Input style={{marginLeft: "10px"}}/>
+                                            </div>
+                                        </Panel>
+                                    </Collapse>
+                                    <Divider orientation="left">Регистрация в OwenCloud</Divider>
+                                    <Collapse size="small">
+                                        <Panel header="Регистрация в OwenCloud" key="1" >
+                                            Пусто
+                                        </Panel>
+                                    </Collapse>
+                                </Modal>
                             <br/>
-                                <div style={{marginRight: "30px", fontSize: "15px", marginLeft: "200px"}}>{role}
+                                <div style={{marginRight: "30px", fontSize: "15px", marginLeft: "35px"}}>
+                                    {role}
                                 </div></div>
                             <NavLink
                                 onClick={handleLogOut}
                                 to="/login"
+                                style={{marginBottom: "20px"}}
                             >
-                                <MeetingRoom style={{marginBottom: "18px"}} />
-                                <div style={{marginBottom: "14px"}}>Выход</div>
-
+                                <MeetingRoom/>
+                                <div>Выход</div>
+                                <br/>
                             </NavLink>
                         </nav>
                     : 'Вы не авторизованы!'
